a9798189c3301b087fa98bc5b3e3652a1fd4cd8e
Scott Pena <scottpena@autoreg-6691809.dyn.wpi.edu>
2025-05-03 23:12:24 -0400

    batch generation\

--- Code/Phase2/train.py
+++ Code/Phase2/train.py
@@ -1,10 +1,83 @@
 import argparse
+import random
+import csv
+import os
+
 import torch
 from torch.optim import Adam
+import numpy as np
+import cv2
 
 from Models.InertialOdometry import InertialOdometry as IO
 from Models.VisualOdometry import VisualOdometry as VO
 from Models.VisualInertialOdometry import VisualInertialOdometry as VIO
+
+
+def generate_IO_batch(batch_size: int):
+    # Need to return (batchsize, 10, 6) set of imu data points
+    batch = []
+    num_trajectories = 0
+    for _, _, files in os.walk('data'):
+        num_trajectories += len(files)
+    traj_idx = random.randint(1, num_trajectories)
+    data_filepath = f'data/traj_{traj_idx}.csv'
+    with open(data_filepath, "r") as file:
+        reader = csv.reader(file)
+        num_rows = len(reader)
+        # subtract 10 since we always want 10 datapoints. prevents index out of bounds error
+        for _ in range(batch_size):
+            starting_idx = random.randint(0, num_rows-10)
+            batch_element = []
+            for i in range(starting_idx, starting_idx+10):
+                row = reader[i]
+                batch_element.append(row)  # input row as a row, so batch is a 2d array
+            batch.append(batch_element)
+    batch_np = np.array(batch)
+    return torch.from_numpy(batch_np)
+
+def generate_VO_batch(batch_size: int):
+    batch = []
+    num_trajectories = 0
+    for _, _, files in os.walk('data'):
+        num_trajectories += len(files)
+    traj_idx = random.randint(1, num_trajectories)
+    data_filepath = f'data/traj_{traj_idx}.csv'
+    with open(data_filepath, "r") as file:
+        reader = csv.reader(file)
+        num_rows = len(reader)
+        for _ in range(batch_size):
+            starting_idx = random.randint(0, num_rows-10)
+            # TODO: Update with image directory filepath
+            image_1 = cv2.imread(reader[starting_idx][-1])
+            image_2 = cv2.imread(reader[starting_idx+10][-1])
+            batch.append([image_1, image_2])
+    batch_np = np.array(batch)
+    return batch_np
+
+def generate_VIO_batch(batch_size):
+    image_batch = []
+    imu_batch = []
+    num_trajectories = 0
+    for _, _, files in os.walk('data'):
+        num_trajectories += len(files)
+    traj_idx = random.randint(1, num_trajectories)
+    data_filepath = f'data/traj_{traj_idx}.csv'
+    with open(data_filepath, "r") as file:
+        reader = csv.reader(file)
+        num_rows = len(reader)
+        for _ in range(batch_size):
+            starting_idx = random.randint(0, num_rows-10)
+            # TODO: Update with image directory filepath
+            image_1 = cv2.imread(reader[starting_idx][-1])
+            image_2 = cv2.imread(reader[starting_idx+10][-1])
+            image_batch.append([image_1, image_2])
+            imu_batch_element = []
+            for i in range(starting_idx, starting_idx+10):
+                row = reader[i]
+                imu_batch_element.append(row)  # input row as a row, so batch is a 2d array
+            imu_batch.append(imu_batch_element)
+    imu_batch_np = np.array(imu_batch)
+    return torch.from_numpy(imu_batch_np), image_batch
 
 def main(args):
     
@@ -12,8 +85,7 @@ def main(args):
     
     if args.mode == 'IO':
         model = IO()
-        optimizer = Adam()
-        lr = 0.001
+        optimizer = Adam(lr=0.001)
         epochs = 200
         batch_size = 32
     elif args.mode == 'VO':
@@ -22,8 +94,7 @@ def main(args):
         for key in weights.keys():
             if key.replace('module.', '') in model.state_dict().keys():
                 model.state_dict()[key.replace('module.', '')] = weights[key]
-        optimizer = Adam()
-        lr = 0.0001
+        optimizer = Adam(lr=0.001)
         epochs = 50
         batch_size = 16
     elif args.mode == 'VIO':
@@ -38,12 +109,68 @@ def main(args):
         for key in lstm_weights.keys():
             if key.replace('lstm.', 'lstm_inertial.') in model.state_dict().keys():
                 model.state_dict()[key.replace('lstm.', 'lstm_inertial.')] = lstm_weights[key]  
-        optimizer = Adam()
-        lr = 0.001
+        optimizer = Adam(lr=0.001)
         epochs = 50
         batch_size = 16
     else:
         raise ValueError(f"Unknown odometry mode. Expected 'IO', 'VO', or 'VIO'. Instead got {args.mode}")
+    
+    start_epoch = 0
+    save_checkpoint = 5
+    
+    for epoch in range(start_epoch, epochs):
+        num_iterations_per_epoch = 5
+        for per_epoch_counter in range(num_iterations_per_epoch):
+            if args.mode == 'IO':
+                batch = generate_IO_batch(batch_size)
+            elif args.mode == 'VO':
+                batch = generate_VO_batch(batch_size)
+            elif args.mode == 'VIO':
+                batch = generate_VIO_batch(batch_size)
+            
+            estimated_pose = model(batch)
+            loss = model.loss(..., estimated_pose) # TODO: Read ground truth and import estimated pose
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            # Save checkpoint every some SaveCheckPoint's iterations
+            if per_epoch_counter % save_checkpoint == 0:
+                # Save the Model learnt in this epoch
+                SaveName = (
+                    "Code/Phase2/checkpoints/"
+                    + str(epoch)
+                    + "a"
+                    + str(per_epoch_counter)
+                    + "model.ckpt"
+                )
+
+                torch.save(
+                    {
+                        "epoch": epoch,
+                        "model_state_dict": model.state_dict(),
+                        "optimizer_state_dict": optimizer.state_dict(),
+                        "loss": loss,
+                    },
+                    SaveName,
+                )
+                print("\n" + SaveName + " Model Saved...")
+            
+        with torch.no_grad():
+            if args.mode == 'IO':
+                batch = generate_IO_batch(batch_size)
+            elif args.mode == 'VO':
+                batch = generate_VO_batch(batch_size)
+            elif args.mode == 'VIO':
+                batch = generate_VIO_batch(batch_size)
+            # val_ims = val_ims.to(cuda)
+            # val_labels = val_labels.to(cuda)
+            # val_corners = val_corners.to(cuda)
+            # print(f"VAL_IDX: {val_idx}")
+            # result = model.validation_step((val_ims, val_labels, val_corners, val_idx))
+        
+        # print(f"Validation Loss: {result['val_loss']}, Training Loss: {epoch_loss}")
+            
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Train models for different modalities")
